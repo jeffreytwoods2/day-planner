7643f40dd34c07a2a88549f3143b06007046622f
GnomeSaiyan <69124994+jeffreytwoods2@users.noreply.github.com>
2020-10-20 14:58:25 -0600

    Added comments to all files

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ var monthList  = ["January", "February", "March", "April", "May", "June", "July"
 var superScript;
 var workHours = [9, 10, 11, 12, 1, 2, 3, 4, 5];
 var button = $("button");
+
+// This var allows init() to auto-populate Notes fields w/ a for loop
 var textBoxes = [
     $("#nine"),
     $("#ten"),
@@ -21,7 +23,7 @@ var textBoxes = [
 ];
 var textBoxVal = [];
 
-
+// Adds appropriate suffix to date
 function superScriptWrite() {
     if (date === 1 || date === 21 || date === 31) {
         superScript = "st"
@@ -34,10 +36,11 @@ function superScriptWrite() {
     };
 };
 
+// If there's a stored note, populates it to the appropriate Notes field
 function init() {
     for (i = 0; i < textBoxes.length; i++) {
         if (localStorage.getItem(textBoxes[i].attr("id")) !== null) {
-            textBoxes[i].text(localStorage.getItem(textBoxes[i].attr("id")))
+            textBoxes[i].text(localStorage.getItem(textBoxes[i].attr("id")));
         };
     };
 };
@@ -45,22 +48,22 @@ function init() {
 init();
 superScriptWrite();
 
+// Displays today's date in a clean format
 today.text(dayList[day] + ", " + monthList[month] + " " + date);
+
+// Appends proper suffix to end of date, superscripted for prettier look
 today.append("<sup>" + superScript + "</sup>");
 
-console.log(date);
-console.log(superScript);
-
+// Save button stores Notes
 button.on('click', function(event) {
     event.stopPropagation();
 
+    // This var goes up to the specfic table row of the button clicked, then goes down to the specific text field in that row
     var input = $(this.parentElement.parentElement.children[1].children[0]);
+    
+    // Gets the ID of the text field next to the button clicked
     var inputAttr = input.attr("id");
+
+    // Sets whatever was typed in Notes to local storage, ties it to the correct text field by giving it same key name as text field's ID
     localStorage.setItem(inputAttr, input.val());
-
-    console.log(localStorage.getItem("nine"));
-
-    console.log(input);
-    console.log("This should be the id of the text box: " + inputAttr);
-    console.log("This should be the text value currently in the field: " + input.val());
 });
